3f17e365fc549c940df0268bf24e1464bb782b36
MichelIwaniec <michel.iwaniec@gmail.com>
2022-05-04 15:08:47 +0100

    WIP: NES version * Edit lcc source files to allow mos6502 as target * Add include/asm/mos6502/types.h (copy-paste of include/asm/sm83/types.h, with BANKED / NONBANKED as no-ops) * Add REENTRANT definition to include/asm/*/types.h, and make it a no-op for non-mos6502 targets * Add 6502 libc routines from sdcc to gbdk-lib/libc/asm/mos6502 * Add implementation for font load / set, 1bpp tile loading, and built-in fonts * Add implementation for 2pp tile loading (converts GB format to NES format on-the-fly) * Add implementation for sprite / metasprite drawing / hiding * Add implementation for joypad reading * Temporarily disable all other builds except Win64 * Temporarily make workflow trigger on push * Enable examples/cross-platform/fonts for NES * Enable examples/cross-platform/metasprites for NES * Enable examples/cross-platform/pong for NES, with work-around to make collisions work (TODO: Investigate this bug!)

--- gbdk-support/lcc/targets.c
+++ gbdk-support/lcc/targets.c
@@ -32,6 +32,19 @@ arg_entry llist0_defaults_msxdos[] = {
     {.searchkey= "_CODE=",       .addflag= "-b",.addvalue= "_CODE=0x0100",       .found= false},
 };
 
+// NES
+arg_entry llist0_defaults_nes[] = {
+    {.searchkey = "CODE=",        .addflag = "-b",.addvalue = "CODE=0x07C000",      .found = false},
+    {.searchkey = "_CODE_1=",     .addflag = "-b",.addvalue = "_CODE_1=0x018000",   .found = false},
+    //{.searchkey = "_CODE_2=",     .addflag = "-b",.addvalue = "_CODE_2=0x028000",   .found = false},
+    //{.searchkey = "_CODE_3=",     .addflag = "-b",.addvalue = "_CODE_3=0x038000",   .found = false},
+    {.searchkey= "CRT0CODE=",    .addflag= "-b",.addvalue= "CRT0CODE=0x07F800",     .found= false},
+    {.searchkey= "VECTORS=",     .addflag= "-b",.addvalue= "VECTORS=0x07F800",      .found= false},
+    {.searchkey= "BSS=",         .addflag= "-b",.addvalue= "BSS=0x400",             .found= false},
+    {.searchkey= "DATA=",        .addflag= "-b",.addvalue= "DATA=0xF0",             .found= false},
+    {.searchkey= "ZP=",          .addflag= "-b",.addvalue = "ZP=0x30",              .found = false},
+};
+
 // Port/Platform specific settings
 //
 // $1 are extra parameters passed using -W
@@ -135,6 +148,23 @@ CLASS classes[] = {
         "%mkbin% $1 $2 $3",
         "%mkcom% $1 $2",
         llist0_defaults_msxdos, ARRAY_LEN(llist0_defaults_msxdos),
+    },
+
+    // NES
+    { "mos6502",        // port
+        "nes",   // plat
+        "nes",   // default_plat
+        EXT_NES, // ROM file extension
+        "%cpp% %cppdefault% -DINT_16_BITS $1 $2 $3",
+        "%includedefault%",
+        "%com% %comdefault% -Wa%asdefault% -DINT_16_BITS $1 %comflag% $2 -o $3",
+        "%as_6500% %asdefault% $1 $3 $2",
+        "%bankpack% -plat=nes $1 $2",
+        "%ld% -n -i -j $1 %libs_include% $3 %crt0dir% $2",
+        "%ihxcheck% $2 $1",
+        "%mkbin% -N -yo 2 -yS $2 $3",
+        "",
+        llist0_defaults_nes, ARRAY_LEN(llist0_defaults_nes),
     }
 
 };
