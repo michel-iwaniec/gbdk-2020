c0a17b0a0e71533cc57026dd394926b6f8230de3
Michel Iwaniec <michel.iwaniec@codegeneration.co.uk>
2023-01-18 23:28:20 +0000

    WIP: Add color attributes to large_map example * Add (reduced-size) color version of map from bbbbbr to cross-platform/large_map/res/bigmap.png * Update Makefile to compile .png to .c / .h files with appropriate platform parameters, and enable build for gbc * Add call to set_bkg_submap_attributes for every set_bkg_submap call * Add #define HAS_ATTRIBUTES to work around SMS/GG attributes issue

--- gbdk-lib/examples/cross-platform/large_map/src/large_map.c
+++ gbdk-lib/examples/cross-platform/large_map/src/large_map.c
@@ -2,8 +2,9 @@
 
 #include <gbdk/platform.h>
 
-#include "bigmap_map.h"
-#include "bigmap_tiles.h"
+#include "res/bigmap.h"
+#define bigmap_mapWidth (bigmap_WIDTH/bigmap_TILE_W)
+#define bigmap_mapHeight (bigmap_HEIGHT/bigmap_TILE_H)
 
 #define camera_max_y ((bigmap_mapHeight - DEVICE_SCREEN_HEIGHT) * 8) 
 #define camera_max_x ((bigmap_mapWidth - DEVICE_SCREEN_WIDTH) * 8) 
@@ -21,6 +22,8 @@
   // For other systems assume height of 240 and adjust Y-scroll 4 pixels down to partly hide artifacts in NTSC overscan
   #define SCROLL_Y_OFFSET 4
 #endif
+
+#define HAS_ATTRIBUTES (!defined(SEGA))
 
 #define MIN(A,B) ((A)<(B)?(A):(B))
 
@@ -61,48 +64,101 @@ inline uint8_t update_row_bottom(uint8_t map_pos_y)
     return map_pos_y + DEVICE_SCREEN_HEIGHT;
 }
 
-void set_camera() {
+void set_camera()
+{
     // update hardware scroll position
     move_bkg(camera_x, WRAP_SCROLL_Y(camera_y + SCROLL_Y_OFFSET));
     // up or down
     map_pos_y = (uint8_t)(camera_y >> 3u);
-    if (map_pos_y != old_map_pos_y) { 
-        if (camera_y < old_camera_y) {
-            set_bkg_submap(map_pos_x,
-                           update_row_top(map_pos_y),
-                           MIN(DEVICE_SCREEN_WIDTH + 1, bigmap_mapWidth-map_pos_x),
-                           1,
-                           bigmap_map,
-                           bigmap_mapWidth);
-        } else {
+    if (map_pos_y != old_map_pos_y)
+    { 
+        if (camera_y < old_camera_y)
+        {
+            set_bkg_submap(
+                map_pos_x,
+                update_row_top(map_pos_y),
+                MIN(DEVICE_SCREEN_WIDTH + 1, bigmap_mapWidth-map_pos_x),
+                1,
+                bigmap_map,
+                bigmap_mapWidth);
+#if HAS_ATTRIBUTES
+            set_bkg_submap_attributes(
+                map_pos_x,
+                update_row_top(map_pos_y),
+                MIN(DEVICE_SCREEN_WIDTH + 1, bigmap_mapWidth-map_pos_x),
+                1,
+                bigmap_map_attributes,
+                bigmap_mapWidth);
+#endif
+        }
+        else
+        {
             if ((bigmap_mapHeight - DEVICE_SCREEN_HEIGHT) > map_pos_y)
-                set_bkg_submap(map_pos_x,
-                               update_row_bottom(map_pos_y),
-                               MIN(DEVICE_SCREEN_WIDTH + 1, bigmap_mapWidth-map_pos_x),
-                               1,
-                               bigmap_map,
-                               bigmap_mapWidth);     
+            {
+                set_bkg_submap(
+                    map_pos_x,
+                    update_row_bottom(map_pos_y),
+                    MIN(DEVICE_SCREEN_WIDTH + 1, bigmap_mapWidth-map_pos_x),
+                    1,
+                    bigmap_map,
+                    bigmap_mapWidth);
+#if HAS_ATTRIBUTES
+                set_bkg_submap_attributes(
+                    map_pos_x,
+                    update_row_bottom(map_pos_y),
+                    MIN(DEVICE_SCREEN_WIDTH + 1, bigmap_mapWidth-map_pos_x),
+                    1,
+                    bigmap_map_attributes,
+                    bigmap_mapWidth);
+#endif
+            }
         }
         old_map_pos_y = map_pos_y; 
     }
     // left or right
     map_pos_x = (uint8_t)(camera_x >> 3u);
-    if (map_pos_x != old_map_pos_x) {
-        if (camera_x < old_camera_x) {
-            set_bkg_submap(update_column_left(map_pos_x),
-                           map_pos_y,
-                           1,
-                           MIN(DEVICE_SCREEN_HEIGHT + 1, bigmap_mapHeight - map_pos_y),
-                           bigmap_map,
-                           bigmap_mapWidth);     
-        } else {
+    if (map_pos_x != old_map_pos_x)
+    {
+        if (camera_x < old_camera_x)
+        {
+            set_bkg_submap(
+                update_column_left(map_pos_x),
+                map_pos_y,
+                1,
+                MIN(DEVICE_SCREEN_HEIGHT + 1, bigmap_mapHeight - map_pos_y),
+                bigmap_map,
+                bigmap_mapWidth);
+#if HAS_ATTRIBUTES
+            set_bkg_submap_attributes(
+                update_column_left(map_pos_x),
+                map_pos_y,
+                1,
+                MIN(DEVICE_SCREEN_HEIGHT + 1, bigmap_mapHeight - map_pos_y),
+                bigmap_map_attributes,
+                bigmap_mapWidth);
+#endif
+        }
+        else
+        {
             if ((bigmap_mapWidth - DEVICE_SCREEN_WIDTH) > map_pos_x)
-                set_bkg_submap(update_column_right(map_pos_x),
-                               map_pos_y,
-                               1,
-                               MIN(DEVICE_SCREEN_HEIGHT + 1, bigmap_mapHeight - map_pos_y),
-                               bigmap_map,
-                               bigmap_mapWidth);
+            {
+                set_bkg_submap(
+                    update_column_right(map_pos_x),
+                    map_pos_y,
+                    1,
+                    MIN(DEVICE_SCREEN_HEIGHT + 1, bigmap_mapHeight - map_pos_y),
+                    bigmap_map,
+                    bigmap_mapWidth);
+#if HAS_ATTRIBUTES
+                set_bkg_submap_attributes(
+                    update_column_right(map_pos_x),
+                    map_pos_y,
+                    1,
+                    MIN(DEVICE_SCREEN_HEIGHT + 1, bigmap_mapHeight - map_pos_y),
+                    bigmap_map_attributes,
+                    bigmap_mapWidth);
+#endif
+            }
         }
         old_map_pos_x = map_pos_x;
     }
@@ -112,12 +168,37 @@ void set_camera() {
 
 void main(){
     DISPLAY_OFF;
-    set_bkg_data(0, 241u, bigmap_tiles);
+    //set_bkg_data(0, 241u, bigmap_tiles);
+    set_native_tile_data(0, bigmap_TILE_COUNT, bigmap_tiles);
+    
+#if defined(SEGA)
+    set_palette(0, bigmap_PALETTE_COUNT, bigmap_palettes);
+#elif defined(GAMEBOY)
+    if (_cpu == CGB_TYPE) {
+        set_bkg_palette(BKGF_CGB_PAL0, bigmap_PALETTE_COUNT, bigmap_palettes);
+    }
+#elif defined(NINTENDO_ENTERTAINMENT_SYSTEM)
+    set_bkg_palette(0, bigmap_PALETTE_COUNT, bigmap_palettes);
+#endif 
 
     map_pos_x = map_pos_y = 0; 
     old_map_pos_x = old_map_pos_y = 255;
-    set_bkg_submap(map_pos_x, map_pos_y, DEVICE_SCREEN_WIDTH, DEVICE_SCREEN_HEIGHT, bigmap_map, bigmap_mapWidth);
-    
+    set_bkg_submap(
+        map_pos_x,
+        map_pos_y,
+        DEVICE_SCREEN_WIDTH,
+        DEVICE_SCREEN_HEIGHT,
+        bigmap_map,
+        bigmap_mapWidth);
+#if HAS_ATTRIBUTES
+    set_bkg_submap_attributes(
+        map_pos_x,
+        map_pos_y,
+        DEVICE_SCREEN_WIDTH,
+        DEVICE_SCREEN_HEIGHT,
+        bigmap_map_attributes,
+        bigmap_mapWidth);
+#endif
     camera_x = camera_y = 0;
     old_camera_x = camera_x; old_camera_y = camera_y;
 
